dff203eaad56108693b36e0164b1a380be0e97e9
Ethan Low <ethanlow223@yahoo.com>
2020-03-07 01:28:48 -0800

    game simulation

--- server/api/home_api.py
+++ server/api/home_api.py
@@ -75,7 +75,7 @@ def create_league():
                 first_name = "SP"
                 last_name = "Pitcher" + str(i + 1)
                 age = 22
-                position = positions[i]
+                position = "SP"
                 contact = 0
                 power = 0
                 velocity = 80
@@ -111,7 +111,171 @@ def generate_scheduele():
                     return jsonify({"error": "failed to schedule game"})
         return jsonify({"success": "generated schedule"})
 
-@home_api.route('/simulate', methods=['POST'])
+@home_api.route('/simulate', methods=['GET', 'POST'])
 def simulate():
     if request.method == 'POST':
         return jsonify({"success": "game completed"})
+    else:
+        import random
+
+        team_1 = Game.query.first().teams[0]
+        team_2 = Game.query.first().teams[1]
+        home_lineup = Player.query.filter_by(team_id=team_1.id).filter(Player.first_name != "SP")
+        home_pitcher = Player.query.filter_by(team_id=team_1.id).filter(Player.first_name == "SP")[0]
+        away_lineup = Player.query.filter_by(team_id=team_2.id).filter(Player.first_name != "SP")
+        away_pitcher = Player.query.filter_by(team_id=team_2.id).filter(Player.first_name == "SP")[0]
+        # GAME SIMULATION CODE
+        # =============================================================================================================================
+        away_score = 0
+        home_score = 0
+        half_inning = 0
+        away_outs = 0
+        home_outs = 0
+        first_base = 0
+        second_base = 0
+        third_base = 0
+        away_plate_appearances = 0
+        home_plate_appearances = 0
+        away_scoreboard = []
+        home_scoreboard = []
+        home_box_score = [
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0}
+        ]
+        away_box_score = [
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0},
+            {"AB": 0, "H": 0, "2B": 0, "3B": 0, "HR": 0, "RBI": 0}
+        ]
+
+        while (away_outs < 27 or away_score == home_score):
+            while (half_inning == 0):
+                batter = away_lineup[away_plate_appearances % 9]
+                away_box_score[away_plate_appearances % 9]["AB"] += 1
+                away_plate_appearances += 1
+                matchup_multiplier = ((batter.contact - home_pitcher.velocity) / 100) + 1
+                pitch = random.randint(0, 1000)
+                swing = batter.contact * 3.6 * matchup_multiplier
+                if (swing >= pitch):
+                    away_box_score[away_plate_appearances % 9]["H"] += 1
+                    if (pitch > 0 and pitch < swing * 0.15):
+                        print("HOME RUN")
+                        away_box_score[away_plate_appearances % 9]["HR"] += 1
+                        rbis = first_base + second_base + third_base + 1
+                        away_box_score[away_plate_appearances % 9]["RBI"] += rbis
+                        away_score += rbis
+                        first_base = 0
+                        second_base = 0
+                        third_base = 0
+                    elif (pitch > swing * 0.15 and pitch < (swing * 0.15) + (swing * 0.02)):
+                        print("TRIPLE")
+                        away_box_score[away_plate_appearances % 9]["3B"] += 1
+                        rbis = first_base + second_base + third_base
+                        away_box_score[away_plate_appearances % 9]["RBI"] += rbis
+                        away_score += rbis
+                        first_base = 0
+                        second_base = 0
+                        third_base = 0
+                    elif (pitch > (swing * 0.15) + (swing * 0.02) and pitch < (swing * 0.15) + (swing * 0.02) + (swing * 0.22)):
+                        print("DOUBLE")
+                        away_box_score[away_plate_appearances % 9]["2B"] += 1
+                        rbis = second_base + third_base
+                        away_box_score[away_plate_appearances % 9]["RBI"] += rbis
+                        away_score += rbis
+                        third_base = first_base
+                        second_base = 1
+                        first_base = 0
+                    else:
+                        print("SINGLE")
+                        rbis = third_base
+                        away_score += rbis
+                        away_box_score[away_plate_appearances % 9]["RBI"] += rbis
+                        third_base = second_base
+                        second_base = first_base
+                        first_base = 1
+                else:
+                    print("OUT")
+                    away_outs += 1
+                    if (away_outs % 3 == 0):
+                        half_inning = 1
+            first_base = 0
+            second_base = 0
+            third_base = 0
+            print("End of top half of inning...")
+            while (half_inning == 1 and (home_outs < 24 or away_score >= home_score)):
+                batter = home_lineup[home_plate_appearances % 9]
+                home_box_score[home_plate_appearances % 9]["AB"] += 1
+                home_plate_appearances += 1
+                matchup_multiplier = ((batter.contact - away_pitcher.velocity) / 100) + 1
+                pitch = random.randint(0, 1000)
+                swing = batter.contact * 3.6 * matchup_multiplier
+                if (swing >= pitch):
+                    home_box_score[home_plate_appearances % 9]["H"] += 1
+                    if (pitch > 0 and pitch < swing * 0.15):
+                        print("HOME RUN")
+                        home_box_score[home_plate_appearances % 9]["HR"] += 1
+                        rbis = first_base + second_base + third_base + 1
+                        home_box_score[home_plate_appearances % 9]["RBI"] += rbis
+                        home_score += rbis
+                        first_base = 0
+                        second_base = 0
+                        third_base = 0
+                    elif (pitch > swing * 0.15 and pitch < (swing * 0.15) + (swing * 0.02)):
+                        print("TRIPLE")
+                        home_box_score[home_plate_appearances % 9]["3B"] += 1
+                        rbis = first_base + second_base + third_base
+                        home_box_score[home_plate_appearances % 9]["RBI"] += rbis
+                        home_score += rbis
+                        first_base = 0
+                        second_base = 0
+                        third_base = 0
+                    elif (pitch > (swing * 0.15) + (swing * 0.02) and pitch < (swing * 0.15) + (swing * 0.02) + (swing * 0.22)):
+                        print("DOUBLE")
+                        home_box_score[home_plate_appearances % 9]["2B"] += 1
+                        rbis = second_base + third_base
+                        home_box_score[home_plate_appearances % 9]["RBI"] += rbis
+                        home_score += rbis
+                        third_base = first_base
+                        second_base = 1
+                        first_base = 0
+                    else:
+                        print("SINGLE")
+                        rbis = third_base
+                        home_score += rbis
+                        home_box_score[home_plate_appearances % 9]["RBI"] += rbis
+                        third_base = second_base
+                        second_base = first_base
+                        first_base = 1
+                else:
+                    print("OUT")
+                    home_outs += 1
+                    if (home_outs % 3 == 0):
+                        half_inning = 0
+            first_base = 0
+            second_base = 0
+            third_base = 0
+            print("End of bottom half of inning...")
+        print("------------------------")
+        print("End of game")
+        print("Final score is away " + str(away_score) + "-" + str(home_score) + " home")
+        print("------------------------")
+        for away_player in away_box_score:
+            print(away_player)
+        print("------------------------")
+        for home_player in home_box_score:
+            print(home_player)
+        # =============================================================================================================================
+        return jsonify(team_1.serialize())
